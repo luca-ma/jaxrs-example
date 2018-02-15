9cea7139cc172efc625b15aa8fa29049d8e02445
luca-ma <lmaderna@gmail.com>
2018-02-15 21:21:08 +0100

    first commit

---
+++ jaxrs-example/src/main/java/it/example/springboot/jaxrsexample/JaxrsExampleApplication.java
@@ -0,0 +1,12 @@
+package it.example.springboot.jaxrsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaxrsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaxrsExampleApplication.class, args);
+	}
+}
